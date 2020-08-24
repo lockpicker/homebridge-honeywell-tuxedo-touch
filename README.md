4a067f11d8862cadc9e9b63512e47b5f442947de
Lockpicker <ashish.bitfield@gmail.com>
2020-08-25 00:18:23 +1000

    Version 1

---
+++ README.md
@@ -0,0 +1,67 @@
+# homebridge-honeywell-tuxedo-touch
+Homebridge plugin for the [Honeywell Tuxedo Touch Wifi security system](https://www.honeywellhome.com/us/en/products/security/security-systems/tuxwifiw-white-tuxedo-touch-7-in-graphic-touchscreen-tuxwifiw/).
+
+This plugin exposes the Honeywell Tuxedo Touch Wifi unit as a security system accessory in Homekit through [Homebridge](http://homebridge.io)
+
+## Installation
+
+1. Install homebridge using: npm install -g homebridge
+2. Install homebridge-honeywell-tuxedo-touch using: npm install -g homebridge-honeywell-tuxedo-touch
+3. Update your configuration file. See sample-config.json in this repository for a sample.
+
+## Features
+This plugin currently only supports the security system feature of the Honeywell Tuxedo Touch unit. It maps the following modes:
+<table style='align:center'>
+  <tr><td><b>Homekit Mode</b></td><td><b>Tuxedo Mode</b></td></tr>
+  <tr><td>Home </td><td>Stay</td></tr>
+  <tr><td>Night </td><td>Night</td></tr>
+  <tr><td>Away </td><td>Away</td></tr>
+</table>
+
+Given enough time and motivation, this can be converted to a platform accessory and expanded to fetch other devices controlled by the Tuxedo unit.
+
+## Configuration
+The configuration options are the following:
+
+```
+"accessories": [
+  {
+    "name": "Home Security",
+    "accessory": "Honeywell Tuxedo Touch",
+    "host": "myalarm.ddns.net",
+    "port": "8000",
+    "alarmCode": "1234",
+    "polling": true,
+    "pollInterval": 10000,
+    "debug": false
+  }
+]
+
+```
+
+- The **name** parameter is optional and determines the name of the security system you will see in HomeKit.
+- The **accessory** parameter tells Homebridge which plugin to load for this accessory.
+- The **host** parameter accepts the ip address or the hostname of the Tuxedo touch unit.
+  Whilst the IP address can be the local LAN ip of the unit, the tuxedo touch unit is sometimes unresponsive when accessed through the lan ip, it seems a lot more reliable when accessed through the WAN interface.
+  If you have a static ip which exposes the unit, you can use that, if not, setup a dynamic dns, expose your tuxedo unit through that and use it with this plugin.
+  Though note that **if your local LAN IP works reliably, that should be your first preference**.
+- The **port** parameter is optional and accepts the port number of the host on which the Tuxedo touch unit is available.
+- The **alarmCode** parameter accepts your security alarm code for arming and disarming the security system.
+- The **polling** is a boolean that specifies if the current state should be pulled on regular intervals or not.
+  This is optional and defaults to false however using this is recommended as it will keep your Homekit status synced with the unit if the state changes outside of a Homekit operation.
+- **pollInterval** is a number which defines the poll interval in milliseconds. Defaults to 30000.
+- The **debug** parameter is boolean and turns on debug messages.
+
+## FAQ
+- Node may throw the following warning
+  ```
+  Warning: Setting the NODE_TLS_REJECT_UNAUTHORIZED environment variable to '0' makes TLS connections and HTTPS requests insecure by disabling certificate verification.
+  ```
+  This is because the Tuxedo units come with really old certs which aren't trusted anymore, without any way to upgrade these certs, the workaround is to not check the cert.
+
+## Dev fuel
+
+<a href="https://www.buymeacoffee.com/lockpicker" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: 41px !important;width: 174px !important;box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;" ></a>
+
+## Credits
+I used the [homebridge-http-securitysystem](https://www.npmjs.com/package/homebridge-http-securitysystem) for while, hacked together for my personal use which inspired me to create a specific plugin for the Tuxedo touch for the benefit of all.
---
+++ index.js
@@ -0,0 +1,330 @@
+'use strict';
+var CryptoJS = require('crypto-js');
+var HTMLParser = require('node-html-parser');
+var request = require('request-promise');
+var pollingtoevent = require("polling-to-event");
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+process.env.NODE_NO_WARNINGS = '1';
+
+let Service, Characteristic;
+
+var protocol = "https";
+var apibasepath = "/system_http_api/API_REV01";
+var hPath = "API_REV01";
+
+let CurrentState = 3;
+let TargetState = 3;
+var api_key_enc;
+var api_iv_enc;
+var lastArmMode = 3;
+
+var alarmStatus = {
+  'Armed Stay'       : 0,
+  'Armed Away'       : 1,
+  'Armed Night'      : 2,
+  'Armed Night Fault': 2,
+  'Ready To Arm'     : 3,
+  'Not Ready Fault'  : 3,
+  'Not Ready Alarm'  : 4,
+  'Armed Stay Alarm' : 4,
+  'Armed Night Alarm': 4,
+  'Armed Away Alarm' : 4
+};
+
+module.exports = (homebridge) => {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerAccessory('homebridge-honeywell-tuxedo-touch', 'Honeywell Tuxedo Touch', HoneywellTuxedoAccessory);
+};
+
+function HoneywellTuxedoAccessory(log, config) {
+      this.log = log;
+      this.config = config;
+      this.debug = config.debug || false;
+      this.polling = config.polling || false;
+      this.pollInterval = config.pollInterval || 30000;
+
+      // extract name from config
+      this.name = config.name || "Honeywell Tuxedo Security";
+
+      this.host = config.host;
+      this.port = config.port || "";
+
+      if(!config.alarmCode){
+        this.log("Alarm code is missing from config");
+      }
+      this.uCode = config.alarmCode;
+
+      // Get API Keys from the tuxedo unit
+      // Create an API request with the cookie jar turned on
+      request = request.defaults({jar: true})
+
+      function getAPIKeys(self){
+          // Create an API request with the cookie jar turned on
+          request = request.defaults({jar: true})
+
+          var tuxApiUrl = "https://" + self.host;
+          if(self.port) tuxApiUrl += ":" + self.port;
+          tuxApiUrl += '/tuxedoapi.html';
+
+          const options = {
+            url: tuxApiUrl,
+            headers: {
+              'User-Agent': 'homebridge'
+            }
+          };
+          request(options)
+            .then(function(body){
+              var root = HTMLParser.parse(body);
+              var readit = root.querySelector('#readit');
+
+              if(readit){
+                self.api_key_enc = readit.getAttribute('value').toString().substr(0,64);
+                self.api_iv_enc = readit.getAttribute('value').toString().substr(64, 96);
+
+                if(self.debug) self.log("[getAPIKeys] Successfully retrieved keys");
+                self.init();
+
+              }else{
+                if(root.querySelector('h1').structuredText == 'Max Number Of Connections In Use.Please Try Again.'){
+                  self.log('[getAPIKeys] Max tuxedo connections exceeded. Please try again in a while.');
+                }
+              }
+            })
+            .catch(function (error) {
+                self.log('[getAPIKeys] Error:', error);
+            });
+      }
+
+      getAPIKeys(this);
+      // create a new Security System service
+      this.SecuritySystem = new Service.SecuritySystem(this.name);
+
+      // create handlers for required characteristics
+      this.SecuritySystem.getCharacteristic(Characteristic.SecuritySystemCurrentState)
+        .on('get', this.handleSecuritySystemCurrentStateGet.bind(this));
+
+      this.SecuritySystem.getCharacteristic(Characteristic.SecuritySystemTargetState)
+        .on('get', this.handleSecuritySystemTargetStateGet.bind(this))
+        .on('set', this.handleSecuritySystemTargetStateSet.bind(this));
+
+      if(this.debug) this.log('Service creation complete');
+  }
+
+HoneywellTuxedoAccessory.prototype = {
+  /**
+   * Init method for regular polling of device state, fired after the api keys have been retrieved
+   */
+  init: function() {
+  	var self = this;
+
+  	// Set up continuous polling if configured
+    console.log("Polling is set to : " + self.polling);
+  	if (self.polling) {
+  		self.log("Starting polling with an interval of %s ms", self.pollInterval);
+
+  		var emitterConfig = [
+  			{
+  				method: self.handleSecuritySystemCurrentStateGet.bind(this),
+  				property: 'current state',
+  				characteristic: Characteristic.SecuritySystemCurrentState
+  			},
+  			{
+  				method: self.handleSecuritySystemTargetStateGet.bind(this),
+  				property: 'target state',
+  				characteristic: Characteristic.SecuritySystemTargetState
+  			}
+  		];
+
+  		emitterConfig.forEach(config => {
+  			var emitter = pollingtoevent(function(done) {
+  				config.method(function (err, result) {
+  					done(err, result);
+  				});
+  			}, { longpolling: true, interval: self.pollInterval });
+
+  			emitter.on("longpoll", function(state) {
+  				self.log('Polling noticed %s change to %s, notifying devices', config.property, state);
+  				self.SecuritySystem
+  						.getCharacteristic(config.characteristic)
+  						.setValue(state);
+  			});
+
+  			emitter.on("error", function(err) {
+  				self.log("Polling of %s failed, error was %s", config.property, err);
+  			});
+  		});
+  	}
+  },
+  getServices: function(){
+    if(this.debug) this.log('Get Services called');
+    if(!this.SecuritySystem) return [];
+
+    const infoService = new Service.AccessoryInformation();
+    infoService.setCharacteristic(Characteristic.Manufacturer,'Honeywell-Tuxedo')
+
+    return [infoService, this.SecuritySystem];
+  },
+  /**
+   * Handle requests to get the current value of the "Security System Current State" characteristic
+   */
+  handleSecuritySystemCurrentStateGet: function(callback) {
+    if(this.debug) this.log.debug('Triggered GET SecuritySystemCurrentState');
+
+    getAlarmMode.apply(this, [returnCurrentState.bind(this)]);
+
+    function returnCurrentState(value){
+        CurrentState  = alarmStatus[JSON.parse(value).Status.toString().trim()];
+        if(this.debug) this.log.debug("[returnCurrentState] Received value" + value)
+        if(this.debug) this.log.debug('[returnCurrentState] Found current state: ' + CurrentState);
+        callback(null, CurrentState);
+    }
+
+  },
+
+  /**
+   * Handle requests to get the current value of the "Security System Target State" characteristic
+   */
+  handleSecuritySystemTargetStateGet: function(callback) {
+    if(this.debug) this.log.debug('Triggered GET SecuritySystemTargetState');
+
+      getAlarmMode.apply(this, [returnTargetState.bind(this)]);
+
+      function returnTargetState(value){
+          var statusString = JSON.parse(value).Status.toString().trim();
+          TargetState  = (alarmStatus[statusString] === undefined) ? this.lastArmMode : alarmStatus[statusString];
+          if(this.debug) this.log.debug("[returnTargetState] Received value" + value)
+          if(this.debug) this.log.debug('[returnTargetState] Found target state: ' + TargetState);
+          callback(null, TargetState);
+      }
+  },
+
+  /**
+   * Handle requests to set the "Security System Target State" characteristic
+   */
+  handleSecuritySystemTargetStateSet: function(value, callback) {
+    if(this.debug) this.log.debug('Triggered SET SecuritySystemTargetState:' + value);
+
+    TargetState = value;
+    this.lastArmMode = value;
+
+    if(value == 0)  armAlarm.apply(this, ['STAY', callback]);
+    if(value == 1)  armAlarm.apply(this, ['AWAY', callback]);
+    if(value == 2)  armAlarm.apply(this, ['NIGHT', callback]);
+    if(value == 3)  disarmAlarm.apply(this, [callback]);
+  }
+}
+
+function callAPI_POST(url, data, paramlength, headers, callback){
+  // Create an API request with the cookie jar turned on
+  request = request.defaults({jar: true})
+  const options = {
+    method: 'POST',
+    url: url,
+    headers: {
+      'authtoken': headers,
+      'identity' : this.api_iv_enc,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: "param=" + data + "&len=" + paramlength + "&tstamp=" + Math.random(),
+    json: true
+  };
+  if(this.debug) this.log.debug("[callAPI_POST]: Calling alarm API with url: " + options.url + " headers - authtoken: " + options.headers['authtoken'] + " headers-identity" + options.headers['identity'] + " body: " + options.body);
+  request.post(options)
+    .then(function(response){
+
+      var resp;
+      try {
+          resp = response["Result"];
+      }
+      catch (e) {
+          try {
+              resp = response.Result;
+          }
+          catch (e) {
+              resp = response.toString();
+          }
+      }
+
+      // At this point, we have the result, so any callbacks can be executed
+      if(this.debug) this.log.debug("[callAPI_POST] Response Final: " + resp);
+
+      callback(decryptData.apply(this, [resp]));
+
+    }.bind(this))
+    .catch(function (error) {
+        this.log('[callAPI_POST] error:', error);
+    }.bind(this));
+}
+
+function getAlarmMode(callback){
+  var url =  "https://" + this.host;
+  if (this.port != "")
+      url += ":" + this.port;
+  url += apibasepath + "/GetSecurityStatus";
+  var header = "MACID:Browser,Path:" + hPath + "/GetSecurityStatus";
+  if(this.debug) this.log.debug("[getAlarmMode] About to call with, url:" + url + " header: " + header + " api_key_enc: " + this.api_key_enc);
+  callAPI_POST.apply(this, [url, "", 0, CryptoJS.HmacSHA1(header, this.api_key_enc), callback]);
+}
+
+function armAlarm(mode, callback){
+  var pID = 1;
+  var dataCnt = encryptData.apply(this, ["arming=" + mode + "&pID=" + pID + "&ucode=" + parseInt(this.uCode) + "&operation=set"]);
+  var url = protocol + "://" + this.host;
+  if (this.port != "")
+      url += ":" + location.port;
+  url += apibasepath + "/AdvancedSecurity/ArmWithCode"; //?param=" + encryptData(dataCnt);
+
+  var header = "MACID:Browser,Path:" + hPath + "/AdvancedSecurity/ArmWithCode";
+  if(this.debug) this.log.debug("[armAlarm] About to call API with, url:" + url + " dataCnt: " + dataCnt + " header: " + header + " api_key_enc: " + this.api_key_enc);
+  callAPI_POST.apply(this, [url, dataCnt, dataCnt.length, CryptoJS.HmacSHA1(header, this.api_key_enc), finishArming]);
+
+  function finishArming(){
+    callback(null);
+  }
+
+}
+
+function disarmAlarm(callback){
+  var pID = 1;
+  var dataCnt = encryptData.apply(this, ["pID=" + pID + "&ucode=" + parseInt(this.uCode) + "&operation=set"]);
+  var url = protocol + "://" + this.host;
+  if (this.port != "")
+       url += ":" + location.port;
+  url += apibasepath + "/AdvancedSecurity/DisarmWithCode"; //?param=" + encryptData(dataCnt);
+
+  var header = "MACID:Browser,Path:" + hPath + "/AdvancedSecurity/DisarmWithCode";
+  if(this.debug) this.log.debug("[disarmAlarm] About to call API with, url:" + url + " dataCnt: " + dataCnt + " header: " + header + " api_key_enc: " + this.api_key_enc);
+  callAPI_POST.apply(this, [url, dataCnt, dataCnt.length, CryptoJS.HmacSHA1(header, this.api_key_enc), finishDisarming]);
+
+  function finishDisarming(value){
+    callback(null);
+  }
+
+}
+
+function decryptData(data) {
+    var encrypted = {};
+    encrypted.ciphertext = CryptoJS.enc.Base64.parse(data);
+    var decrypted = CryptoJS.AES.decrypt(encrypted, CryptoJS.enc.Hex.parse(this.api_key_enc),
+    {
+        iv: CryptoJS.enc.Hex.parse(this.api_iv_enc),
+        salt: "",
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    });
+    if(this.debug) this.log.debug("[decryptData] Returning: " +  decrypted.toString(CryptoJS.enc.Latin1));
+    return decrypted.toString(CryptoJS.enc.Latin1);
+}
+
+function encryptData(data) {
+    var encString = CryptoJS.AES.encrypt(data, CryptoJS.enc.Hex.parse(this.api_key_enc),
+    {
+        iv: CryptoJS.enc.Hex.parse(this.api_iv_enc),
+        salt: "",
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    });
+    return encodeURIComponent(encString);
+}
